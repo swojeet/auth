d311cbe4eca94950bf5c68165cac4e089522ca84
swojeet <swojeetkayastha@gmail.com>
2018-11-20 16:40:28 +0545

    Create LoginForm component, initialize firebase with lifecycle methods,

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
+import firebase from '@firebase/app';
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import { Header } from './components/common'
+import { View } from 'react-native';
+import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
-  render(){
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyD15kGwvGv3vifv44x6G_RVVRCoTiprZNk',
+      authDomain: 'auth-4bbbb.firebaseapp.com',
+      databaseURL: 'https://auth-4bbbb.firebaseio.com',
+      projectId: 'auth-4bbbb',
+      storageBucket: 'auth-4bbbb.appspot.com',
+      messagingSenderId: '552619958187'
+    });
+  }
+
+  render() {
     return (
       <View>
-        <Header headerText="Authentication"></Header>
-        <Text>An App</Text>
+        <Header headerText="Authentication" />
+        <LoginForm />
       </View>
     );
   }
---
+++ src/components/LoginForm.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { TextInput } from 'react-native';
+import { Button, Card, CardSection } from './common';
+
+class LoginForm extends Component {
+  state = { text: '' };
+
+  render() {
+    return (
+      <Card>
+        <CardSection>
+          <TextInput
+            value={this.state.text}
+            onChangeText={text => this.setState({ text })}
+            style={{ height: 20, width: 100, paddingVertical: 0 }}
+          />
+        </CardSection>
+        <CardSection>
+
+        </CardSection>
+
+        <CardSection>
+          <Button>
+            Login
+          </Button>
+        </CardSection>
+      </Card>
+    );
+  }
+}
+
+export default LoginForm;
